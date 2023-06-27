86cd363bd9677ba7e3ee7fb2500645162cd12ba1
Nurkhat Jumabaev <nurkhatjumabaev@arizona.edu>
2023-06-27 13:09:42 +0500

    last

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ function Contact(){
       <br />
       <br />
       <h1 className="text-info" id="contact">Contact Me</h1>
-      <p className="mx-auto" style={{width: "35rem", lineHeight: "3rem"}}>If you are interested in working with me, just send me an email. <br/> I will respond it as soon as I can.</p>
+      <p className="mx-auto" style={{lineHeight: "3rem"}}>If you are interested in working with me, just send me an email. <br/> I will respond it as soon as I can.</p>
       <a className="btn btn-primary" href="mailto:nurkhatjumabaev@arizona.edu" target={'_blank'}>Email</a>
       <br />
       <br />
